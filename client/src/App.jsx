60e30aab9f0cd3a9cf9ec89f18726184fdef954a
Paakwesi1972 <c3ntury.18@gmai.com>
2026-08-21 10:10:42 +0000

    Add smooth page transition animations

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Splash from './pages/Splash';
 import Welcome from './pages/auth/Welcome';
 import Login from './pages/auth/Login';
@@ -24,32 +24,36 @@ import Filters from './pages/Filters';
 import Notifications from './pages/Notifications';
 
 export default function App() {
+  const location = useLocation();
+
   return (
-    <Routes>
-      <Route path="/" element={<Splash />} />
-      <Route path="/welcome" element={<Welcome />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/vendor-signup" element={<VendorSignUp />} />
-      <Route path="/verify-otp" element={<OtpVerification />} />
-      <Route path="/forgot-password" element={<ForgotPassword />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/listing/:id" element={<ListingDetail />} />
-      <Route path="/listing/:id/edit" element={<EditListing />} />
-      <Route path="/messages" element={<Messages />} />
-      <Route path="/messages/:listingId" element={<Chat />} />
-      <Route path="/messages/thread/:conversationId" element={<Chat />} />
-      <Route path="/sell" element={<CreateListing />} />
-      <Route path="/vendor-registration" element={<VendorRegistration />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/listing/:id/review" element={<RatingsReviews />} />
-      <Route path="/admin" element={<AdminDashboard />} />
-      <Route path="/my-listings" element={<MyListings />} />
-      <Route path="/saved" element={<SavedItems />} />
-      <Route path="/settings" element={<AccountSettings />} />
-      <Route path="/support" element={<HelpSupport />} />
-      <Route path="/filters" element={<Filters />} />
-      <Route path="/notifications" element={<Notifications />} />
-    </Routes>
+    <div key={location.pathname} className="animate-[pageFade_0.28s_ease-out]">
+      <Routes location={location}>
+        <Route path="/" element={<Splash />} />
+        <Route path="/welcome" element={<Welcome />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/vendor-signup" element={<VendorSignUp />} />
+        <Route path="/verify-otp" element={<OtpVerification />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/listing/:id" element={<ListingDetail />} />
+        <Route path="/listing/:id/edit" element={<EditListing />} />
+        <Route path="/messages" element={<Messages />} />
+        <Route path="/messages/:listingId" element={<Chat />} />
+        <Route path="/messages/thread/:conversationId" element={<Chat />} />
+        <Route path="/sell" element={<CreateListing />} />
+        <Route path="/vendor-registration" element={<VendorRegistration />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/listing/:id/review" element={<RatingsReviews />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/my-listings" element={<MyListings />} />
+        <Route path="/saved" element={<SavedItems />} />
+        <Route path="/settings" element={<AccountSettings />} />
+        <Route path="/support" element={<HelpSupport />} />
+        <Route path="/filters" element={<Filters />} />
+        <Route path="/notifications" element={<Notifications />} />
+      </Routes>
+    </div>
   );
 }
